37dde8579f9229d54ca61c181caa7c80ccdc9812
Miguel Catalán <miguelalejandro28@gmail.com>
2025-08-29 22:04:06 -0600

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name, price, category, created_at)
+VALUES 
+  ('Audífonos', 299.99, 'Electrónica', CURRENT_TIMESTAMP),
+  ('Lámpara de escritorio', 149.50, 'Hogar', CURRENT_TIMESTAMP),
+  ('Cafetera', 450.00, 'Cocina', CURRENT_TIMESTAMP),
+  ('Mochila universitaria', 350.00, 'Accesorios', CURRENT_TIMESTAMP);
